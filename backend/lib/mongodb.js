795707341bc313cde08d87ca6b9568d70d8e8e7c
Rakesh <kondelarakesh123@gmail.com>
2026-05-23 14:13:47 +0530

    Day 4 Speech-to-Text API integration completed

---
+++ backend/lib/mongodb.js
@@ -0,0 +1,15 @@
+// import mongoose from "mongoose";
+// const MONGO_URI = process.env.MONGO_URI
+
+
+// async function connectDB(){
+//     if(mongoose.connections[0].readyState){
+//         return
+
+//     }
+        
+
+// await mongoose.connect(MONGO_URI)
+// }
+
+// export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express")
 const cors = require("cors")
 const multer = require("multer")
+const mongoose = require("mongoose")
+const fs = require("fs")
+const OpenAI = require("openai")
+const dotenv = require("dotenv")
+
 const app =express()
 
 app.use(cors())
 app.use(express.json())
-
-
+dotenv.config()
+const clinet = new OpenAI({
+    apikey:process.env.OPENAI_API_KEY,
+})
+//storage multer...//
 const storage = multer.diskStorage({
     destination:(req, file, cb) =>{
         cb(null, "uploads/")
@@ -17,20 +25,38 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage})
+///upload rote..///
+app.post("/upload", upload.single("audio"), async (req, res) => {
+    try{
+        const transcription = await client.audio.transcriptions.create({
+            file:fs.createReadStream(req.file.path),
+            model:"whisper-1",
+        })
+        res.json({
+            message:"File upladed successfully",
+            transcription:transcription.text
+        })
+    }catch (error) {
+        console.log(error)
 
-app.post("/upload", upload.single("audio"), (req, res) => {
-    res.json({
-        message:"File uploaded successfully",
-        file: req.file,
-    })
+        res.status(500).json({
+            error : "Transcription failed"
+        })
+    }
+})
+    
+//mongodb connection...
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDb conneted Successfully")
+})
+.catch((error)=>{
+    console.log(error)
 })
 
 
-
-
-
 const port = 5000;
-
+//test route..//
 
 app.get("/", (req, res)=>{
     res.send("BAckend server is running")
